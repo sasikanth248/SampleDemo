2a6ba3013aa5f8acb26487b03496df8e949610bb
sasikanth <sasikanth@krishtipati>
2018-01-15 17:50:25 +0530

    added class four

---
+++ SampleDemo/src/main/java/Sample4.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author sasikanth
+ *
+ */
+public class Sample4 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
