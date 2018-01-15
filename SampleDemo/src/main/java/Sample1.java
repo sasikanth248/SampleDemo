430d243ab12a486d47725164513e11e8494c5f7b
sasikanth <sasikanth@krishtipati>
2018-01-15 16:16:57 +0530

    Another git example

---
+++ SampleDemo/src/main/java/Sample1.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * @author sasikanth
+ *
+ */
+public class Sample1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Example1");
+	}
+
+}
